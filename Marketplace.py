59df4010438d766416dd7b7f751cbab7f16138f8
MikeNordMan <49362861+MikeNordMan@users.noreply.github.com>
2022-06-25 09:30:06 +0500

    add class Marketplace.py add class Traider.py

---
+++ Marketplace.py
@@ -0,0 +1,14 @@
+class Marketplase:
+    '''Описание класса'''
+    def __init__(self, name, url):
+        self.nameMarket = name
+        self.urlMarket = url
+
+
+    def testClass(self):
+        print(self.nameMarket)
+        print(self.urlMarket)
+
+
+
+
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from Marketplace import Marketplase
+from Traider import Tr
+
 
 def main():
-    pass
+    print('Start Main:')
+    mp = Marketplase('Traiding','www.tr')
+    mp.testClass()
+    print('___________________________________')
+    mp2 = Tr('LME', 'www.lme.com', 'проверка')
+    mp2.pasrsing()
+    mp2.testClass()
+    print(mp2.nameMarket)
 
 
 
